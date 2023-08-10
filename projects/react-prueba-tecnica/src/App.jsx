320dbc5e91836296ae596fe17300ee833495d4ca
Carlos Oz <carlos.oz.fi@gmail.com>
2023-08-10 15:08:11 -0600

    test and custom hooks

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,51 +1,24 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
-  const [fact, setFact] = useState()
-  const [imageUrl, setImageUrl] = useState()
-  const [factError, setFactError] = useState()
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({fact})
 
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(response => {
-        if (!response.ok) {
-          setFactError('Error fetching fact')
-        }
-        return response.json()})
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-
-      })
-
-  }, [])
-
-  useEffect(() => {
-    if (!fact) return
-    const threeFirstWords = fact.split(' ', 3).join(' ')
-    console.log(threeFirstWords)
-
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
-
+  const handleGetNewFact = async () => {
+    refreshFact()
+  }
   return (
     <main >
       <h1>App de gatitos</h1>
+      <button onClick={handleGetNewFact}>Get new fact</button>
 
-      <section>
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
-      </section>
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+
+      {/* <Otro/> */}
     </main>
 
   )
